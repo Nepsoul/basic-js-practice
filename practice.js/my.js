4a194e64c582757ad9d4dcdee6235abceb7a1233
Niru Magar <nirumagar901@gmail.com>
2022-08-11 14:08:39 +0545

    feat: object

---
+++ practice.js/my.js
@@ -0,0 +1,21 @@
+let person ={ 
+    name: 'manzila',
+    age: 23
+}
+person.addr = {city: 'ktm'}
+person['school']={name: 'TU'}
+
+person['school'] = {name: {
+    short: 'TU',
+    long: 'Trivuwan University'
+}
+}
+ console.log(person);
+ let shortName = person.school.name.short //to declare new value and assigning to value to it.
+ console.log(shortName)
+
+ //to change the value of key //reassign
+ person.school.name.short = 'Ascol'
+ console.log(person);
+
+ 
