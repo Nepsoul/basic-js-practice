c3fa536e027c34b525b6d4957274ce7defd3b404
Niru Magar <nirumagar901@gmail.com>
2022-09-07 10:04:17 +0545

    chore: hollow array

--- practice/hollow.js
+++ practice/hollow.js
@@ -6,7 +6,7 @@ Write a function named isHollow/is_hollow/IsHollow that accepts an integer array
 returns true if it is a hollow array,else false.
 
 ===>e.g.<===
-[1,2,000,4,5]=>hollow array true
+[1,2,0,0,0,4,5]=>hollow array true
 [1,2,3,0,0,0,0,0,4,5,6]=>true
 [-1,000,4]=>true
 [0,0,0]=>true
@@ -19,11 +19,38 @@ returns true if it is a hollow array,else false.
 
 =====>pseudocode<=======
 1. go through the list of element in an array
-2. check if there is presence of zero in the middle of array or not.
-3. check the number of zero(atleast 3) in mid of array.
+2. check if there is presence of zero (atleast 3) in the middle of array or not.
 4. check if the no. of non-zero elements before and after zero are equal.
 */
 
+
 function isHollow(x){
+    let preCount=0
+    let midCount=0
+    let postCount=0
+    let zeroCounted=false  //flag
+    
+for(let i=0; i<x.length;i++){
+    let currVal=x[i]
+    
+    if(currVal===0){
+        midCount++
+        zeroCounted =true
+    }else{
+
+        if(zeroCounted){
+            postCount++
+        }else{
+            preCount++
+        }
+    }
+    }
+    console.log(preCount,midCount,postCount)
+    if(preCount===postCount && midCount >2){
+        return true
+    }else{
+        return false
+    }
 
 }
+isHollow([0,4,2,0,0,0,4,5])
